9f5ad1f140dc617e9a3d17dd727c219e1aae9961
totovr <a.vegaramirez76@gmail.com>
2018-03-13 12:00:19 +0900

    Update

--- Video/Video.py
+++ Video/Video.py
@@ -3,8 +3,10 @@ from time import sleep
 
 camera = PiCamera()
 
+try:
 camera.start_preview()
-camera.start_recording('/home/pi/Desktop/Video/video.h264')
+camera.start_recording('/home/pi/Videos/Pi_Videos/video.h264')
 sleep(10)
 camera.stop_recording()
+finally:
 camera.stop_preview()
